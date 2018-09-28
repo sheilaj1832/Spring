cfa1086767324d68265adb4885d03d7d6a72489e
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-09-27 21:10:27 -0400

    Spring projects frm Sept 26 & 27

---
+++ HelloWorldSpringWebMySQL/src/main/java/com/hello/HelloWorldSpringWebMySqlApplication.java
@@ -0,0 +1,12 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringWebMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringWebMySqlApplication.class, args);
+	}
+}
